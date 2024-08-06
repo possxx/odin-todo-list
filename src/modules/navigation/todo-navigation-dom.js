56e5932a6a3dc5bb194d037ea389fc88729544eb
possxx <168581964+possxx@users.noreply.github.com>
2024-08-06 17:28:00 +0200

    Add eventListener to todo edit icon

--- src/modules/navigation/todo-navigation-dom.js
+++ src/modules/navigation/todo-navigation-dom.js
@@ -1,7 +1,7 @@
 import { projects } from "./project-navigation.js";
 import { createTodo, removeAllTodos, removeTodo } from "./todo-navigation.js";
 import { updateAttribute } from "./project-navigation-dom.js";
-import { createInitialTodoEditContentElement, content, contentChild } from "../content/todo-edit-content.js";
+import { createInitialTodoEditContentElement, createTodoEditContentElement, content, contentChild } from "../content/todo-edit-content.js";
 
 export { createTodoDom, removeAllTodosDom };
 
@@ -31,6 +31,7 @@ function createTodoDomElement(projectIndex, todoIndex) {
     todoEditIconSvg.setAttribute("viewBox", "0 -960 960 960");
     todoEditIconPath.setAttribute("d", "M186.67-186.67H235L680-631l-48.33-48.33-445 444.33v48.33ZM120-120v-142l559.33-558.33q9.34-9 21.5-14 12.17-5 25.5-5 12.67 0 25 5 12.34 5 22 14.33L821-772q10 9.67 14.5 22t4.5 24.67q0 12.66-4.83 25.16-4.84 12.5-14.17 21.84L262-120H120Zm652.67-606-46-46 46 46Zm-117 71-24-24.33L680-631l-24.33-24Z");
     todoEditIconSvg.appendChild(todoEditIconPath);
+    todoEditIconSvg.addEventListener("click", () => createTodoEditContentElement(projectIndex, todoIndex));
 
     const todoDeleteIconSvg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
     const todoDeleteIconPath = document.createElementNS("http://www.w3.org/2000/svg", "path");
