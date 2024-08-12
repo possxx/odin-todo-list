81dc58716a13db9a54992bd809bd3b89ae91e834
possxx <168581964+possxx@users.noreply.github.com>
2024-08-12 19:09:03 +0200

    Add eventListeners to navigation

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
-console.log( "Hello World" );
+import { renderTodayContent } from "./modules/content/today-render-content";
+import { renderUpcomingContent } from "./modules/content/upcoming-render-content";
+import { renderPriorityContent } from "./modules/content/priority-render-content";
+
+const todayNavigation = document.querySelector(".today");
+todayNavigation.addEventListener("click", () => renderTodayContent());
+
+const upcomingNavigation = document.querySelector(".upcoming");
+upcomingNavigation.addEventListener("click", () => renderUpcomingContent());
+
+const highPriorityNavigation = document.querySelector(".high-priority");
+highPriorityNavigation.addEventListener("click", () => renderPriorityContent("high"));
+
+const mediumPriorityNavigation = document.querySelector(".medium-priority");
+mediumPriorityNavigation.addEventListener("click", () => renderPriorityContent("medium"));
+
+const lowPriorityNavigation = document.querySelector(".low-priority");
+lowPriorityNavigation.addEventListener("click", () => renderPriorityContent("low"));
